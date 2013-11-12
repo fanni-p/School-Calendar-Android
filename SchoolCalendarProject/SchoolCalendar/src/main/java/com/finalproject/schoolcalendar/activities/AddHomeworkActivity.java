c427c66556fee01859357196c3b65e1cf05cc724
fanni-p <fanni@abv.bg>
2013-11-12 13:40:34 +0200

    Add TimetableAPI Web Services

---
+++ SchoolCalendarProject/SchoolCalendar/src/main/java/com/finalproject/schoolcalendar/activities/AddHomeworkActivity.java
@@ -0,0 +1,13 @@
+package com.finalproject.schoolcalendar.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Fani on 11/17/13.
+ */
+public class AddHomeworkActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
