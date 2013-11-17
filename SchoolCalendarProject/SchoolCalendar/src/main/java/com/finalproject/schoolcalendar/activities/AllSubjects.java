28aaa339a8d564d9dce93752e126ed597764f314
fanni-p <fanni@abv.bg>
2013-11-17 02:52:19 +0200

    Add Toggle Context menu to Subjects list

--- SchoolCalendarProject/SchoolCalendar/src/main/java/com/finalproject/schoolcalendar/activities/AllSubjects.java
+++ SchoolCalendarProject/SchoolCalendar/src/main/java/com/finalproject/schoolcalendar/activities/AllSubjects.java
@@ -7,11 +7,13 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.support.v4.app.FragmentActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.finalproject.schoolcalendar.R;
 import com.finalproject.schoolcalendar.adapters.SubjectsArrayAdapter;
@@ -88,15 +90,40 @@ public class AllSubjects extends ListActivity
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        if (view.getId() == android.R.id.list) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            menu.setHeaderTitle(this.mAllSubjects[info.position].getName());
+            String[] menuItems = getResources().getStringArray(R.array.edit_delete_menu);
+            for (int i = 0; i < menuItems.length; i++) {
+                menu.add(Menu.NONE, i, i, menuItems[i]);
+            }
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_logout:
-//                handleLogoutCommand();
-//                return true;
-//            default:
-        this.mNavigationDrawerManager.handleOnOptionsItemSelected(item);
-        return super.onOptionsItemSelected(item);
-//        }
+        switch (item.getItemId()) {
+            case R.id.action_add_subject:
+                this.handleAddSubjectCommand();
+                return true;
+            default:
+                this.mNavigationDrawerManager.handleOnOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //TODO Handle on item select
+//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+//        int menuItemIndex = item.getItemId();
+//        String[] menuItems = getResources().getStringArray(R.array.edit_delete_menu);
+//        String menuItemName = menuItems[menuItemIndex];
+//        String listItemName = this.mAllSubjects[info.position].getName();
+//
+//        Toast.makeText(this, String.format("Selected %s for item %s", menuItemName, listItemName), Toast.LENGTH_LONG);
+        return true;
     }
 
     @Override
@@ -131,16 +158,24 @@ public class AllSubjects extends ListActivity
     private void handleGetAllSubjectsResponse(HttpResponseHelper response) {
         if (response.isStatusOk()) {
             this.mAllSubjects = this.mGson.fromJson(response.getMessage(), SubjectModel[].class);
-
             AllSubjects.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    AllSubjects.this.mSubjectArrayAdapter = new SubjectsArrayAdapter(AllSubjects.this,
-                            R.layout.subjectlist_item_row, AllSubjects.this.mAllSubjects);
-                    AllSubjects.this.mSubjectsList = (ListView) findViewById(android.R.id.list);
-                    AllSubjects.this.mSubjectsList.setAdapter(AllSubjects.this.mSubjectArrayAdapter);
+                    loadListView();
                 }
             });
         }
+    }
+
+    private void loadListView() {
+        this.mSubjectArrayAdapter = new SubjectsArrayAdapter(this,
+                R.layout.subjectlist_item_row, this.mAllSubjects);
+        this.mSubjectsList = (ListView) findViewById(android.R.id.list);
+        this.mSubjectsList.setAdapter(this.mSubjectArrayAdapter);
+        this.registerForContextMenu(this.mSubjectsList);
+    }
+
+    private void handleAddSubjectCommand() {
+
     }
 }
